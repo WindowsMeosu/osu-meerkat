676c2ed43700649935c6566b90884cca91b7e45b
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2023-01-02 19:22:43 +0100

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <meerkatdirs.h>
+static cho
+server_check ( cho.ppy.sh, checkifvalid, connect serv)
+}
+
+      
+static void
+connectcho (server * mp_num)
+{
+ dm.banchobot
+   create.mp.chan
+    join.mp.chan
+      prevent.silences
+ if connectcho = false then connectirc ( irc.ppy.sh, checkifvalid, connect serv
+                                        }
