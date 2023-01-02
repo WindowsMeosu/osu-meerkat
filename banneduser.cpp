5a4c464febea94fb569384cf0ad89eeedeb319e7
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2023-01-02 19:45:00 +0100

    q

---
+++ banneduser.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <kat/dir.h>
+
+
+
+static int
+user_kick_action = true
+    user_ban_action = false
+{
+	 perform kick;
+	return TRUE;
+}
+
+void
+msgUser log("you have kicked been due to excessive kicking, please do run/report <user> next time")
+{
+	textPrint log ("%u has been kicked for excessive kicking")
+}
+if user_ban_action = true then perform ban
+return true
+    msgUser log("as a result of banning users from this room, you have been restricted access to any rooms using osu!meerkat until further notice")
